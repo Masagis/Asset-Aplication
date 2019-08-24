f628974d81f21bd4372cdcef011830b56607db72
Agis Tri Wahyuji <31729034+Masagis@users.noreply.github.com>
2019-08-24 17:04:17 +0700

    intro
    intro uload file

---
+++ views/kategori.php
@@ -0,0 +1,241 @@
+<?php
+session_start();
+include_once "../base_url.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>
+    Dashboard Admin - Kategori 
+  </title>
+  <!-- Favicon -->
+  <link href="<?php echo $base_url ?>assets/img/brand/favicon.png" rel="icon" type="image/png">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <!-- Icons -->
+  <link href="<?php echo $base_url ?>assets/js/plugins/nucleo/css/nucleo.css" rel="stylesheet" />
+  <link href="<?php echo $base_url ?>assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="<?php echo $base_url ?>assets/css/argon-dashboard.css?v=1.1.0" rel="stylesheet" />
+</head>
+
+<body class="">
+  <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+    <div class="container-fluid">
+      <!-- Toggler -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Brand -->
+      <a class="navbar-brand pt-0" href="index.php">
+        <img src="<?php echo $base_url ?>assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+      </a>
+
+      <!-- Collapse -->
+      <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+        <!-- Collapse header -->
+        <div class="navbar-collapse-header d-md-none">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a href="index.php">
+                <img src="<?php echo $base_url ?>assets/img/brand/blue.png">
+              </a>
+            </div>
+            <div class="col-6 collapse-close">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        
+        <!-- Navigation -->
+        <ul class="navbar-nav">
+          <li class="nav-item " class="active" >
+          <a class=" nav-link " href="index.php"> <i class="ni ni-tv-2 text-primary"></i> Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="kategori.php">
+              <i class="ni ni-collection text-blue"></i> Kategori
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="aset.php">
+              <i class="ni ni-building text-orange"></i> Aset
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="penyusutan.php">
+              <i class="ni ni-single-copy-04 text-yellow"></i> Penyusutan
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="laporan.php">
+              <i class="ni ni-archive-2 text-red"></i> Laporan
+            </a>
+          </li>
+        </ul>
+        <!-- Divider -->
+        <hr class="my-3">
+      </div>
+    </div>
+  </nav>
+
+    <div class="main-content">
+      <!-- Navbar -->
+      <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
+        <div class="container-fluid">
+          
+          <!-- Brand -->
+          <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" >Kategori</a>
+  
+          <!-- User -->
+          <ul class="navbar-nav align-items-center d-none d-md-flex">
+            <li class="nav-item dropdown">
+              <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="media align-items-center">
+                  <span class="avatar avatar-sm rounded-circle">
+                    <img alt="Image placeholder" src="<?php echo $base_url ?>assets/img/theme/team-4-800x800.jpg">
+                  </span>
+                  <div class="media-body ml-2 d-none d-lg-block">
+                    <span class="mb-0 text-sm  font-weight-bold">Admin</span>
+                  </div>
+                </div>
+              </a>
+              <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+                <div class=" dropdown-header noti-title">
+                  <!-- <h6 class="text-overflow m-0">Welcome!</h6> -->
+                </div>
+                <div class="dropdown-divider"></div>
+                <a href="#!" class="dropdown-item">
+                  <i class="ni ni-user-run"></i>
+                  <span>Logout</span>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- End Navbar -->
+  
+    <!-- Header -->
+    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+      <div class="container-fluid">
+        <div class="header-body">
+          <!-- Card stats -->
+          <div class="row">
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <a type="button" href="kategori.php" class="btn btn-secondary btn-lg">KATEGORI</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+                <div class="card card-stats mb-4 mb-xl-0">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col">
+                        <a type="button" href="aset.php"class="btn btn-secondary btn-lg">ASET</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-6">
+                  <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col">
+                          <a type="button" href="penyusutan.php"class="btn btn-secondary btn-lg">PENYUSUTAN</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                      <div class="card-body">
+                        <div class="row">
+                          <div class="col">
+                            <a type="a" href="laporan.php" class="btn btn-secondary btn-lg">LAPORAN</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      <!-- engd Header -->
+  
+    <div class="container-fluid mt--7">
+            <!-- Table -->
+            <div class="row">
+                <div class="col">
+                  <div class="card shadow">
+                    <div class="card-header border-0">
+                      <h3 class="mb-0">SELAMAT DATANG</h3>
+                    </div>
+                    <div class="table-responsive">
+                      <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                          <tr>
+                            <th scope="col">Project</th>
+                            <th scope="col">Budget</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Users</th>
+                            <th scope="col">Completion</th>
+                            <th scope="col"></th>
+                          </tr>
+                        </thead>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+      <!-- Footer -->
+      <footer class="footer">
+          <div class="row align-items-center justify-content-xl-between">
+            <div class="col-xl-6">
+              <div class="copyright text-center text-xl-left text-muted">
+                &copy; 2019 <a class="font-weight-bold ml-1">Silent Tim</a>
+              </div>
+            </div>
+          </div>
+        </footer>
+    </div>
+  </div>
+  <!-- End Footer -->
+  
+  
+  <!--   Core   -->
+  <script src="<?php echo $base_url ?>assets/js/plugins/jquery/dist/jquery.min.js"></script>
+  <script src="<?php echo $base_url ?>assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <!--   Optional JS   -->
+  <!--   Argon JS   -->
+  <script src="<?php echo $base_url ?>assets/js/argon-dashboard.min.js?v=1.1.0"></script>
+  <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+  <script>
+    window.TrackJS &&
+      TrackJS.install({
+        token: "ee6fab19c5a04ac1a32a645abde4613a",
+        application: "argon-dashboard-free"
+      });
+  </script>
+</body>
+
+</html>
