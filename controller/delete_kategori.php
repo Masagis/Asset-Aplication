76927dddf6f6bbb7221b548debfac9d120131b92
Doni Agus Adila <pangerancleeper@gmail.com>
2019-08-31 17:06:24 +0700

    Update delete_kategori.php

--- controller/delete_kategori.php
+++ controller/delete_kategori.php
@@ -6,5 +6,7 @@ include_once("../config/config.php");
         // Hapus data
         $result = mysqli_query($mysqli, "DELETE FROM tb_kategori WHERE id_kategori = '$id'");
         header("location: ../pages/table-kategori.php"); 
+    }else{
+    	echo "gagal";
     }
 ?>
