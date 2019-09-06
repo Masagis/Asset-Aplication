beb0170734c0d2396899e26445a7e8c6173ee5d6
Masagis <31729034+Masagis@users.noreply.github.com>
2019-09-06 21:21:25 +0700

    update database and drop yang tidak di gunakan
    memperbaharui  database dan menghapus source yang tidak di gunakan

--- db_assets.sql
+++ db_assets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2019 at 02:40 PM
+-- Generation Time: Sep 06, 2019 at 04:19 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -30,15 +30,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_asset` (
   `id_asset` varchar(18) NOT NULL,
-  `nama_asset` varchar(25) NOT NULL,
-  `qty` int(12) NOT NULL,
+  `nopol` varchar(25) NOT NULL,
+  `kete_aset` varchar(25) NOT NULL,
   `tgl_perolehan` date NOT NULL,
-  `hrg_perolehan` int(12) NOT NULL,
+  `hrg_baku` int(12) NOT NULL,
   `umur_ekonomis` int(12) NOT NULL,
   `nilai_sisa` int(12) NOT NULL,
-  `nilai_susut` int(128) NOT NULL,
-  `id_kategori` varchar(18) NOT NULL
+  `nilai_susut` int(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_asset`
+--
+
+INSERT INTO `tb_asset` (`id_asset`, `nopol`, `kete_aset`, `tgl_perolehan`, `hrg_baku`, `umur_ekonomis`, `nilai_sisa`, `nilai_susut`) VALUES
+('R2-003', 'BE 7789 MA', 'Kijangan', '2019-08-25', 23000000, 45, 2000, 0),
+('R4-001', 'be 3454 hj', 'vario M125', '2019-09-03', 23000000, 2, 200000, 0),
+('R4-002', 'BE 7789 MA', 'Kijangan', '2019-09-03', 23000000, 7, 3000000, 0);
 
 -- --------------------------------------------------------
 
@@ -48,8 +56,17 @@ CREATE TABLE `tb_asset` (
 
 CREATE TABLE `tb_kategori` (
   `id_kategori` varchar(18) NOT NULL,
-  `nm_katagori` varchar(25) NOT NULL
+  `nopol` varchar(62) NOT NULL,
+  `kete_kategori` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id_kategori`, `nopol`, `kete_kategori`) VALUES
+('R2', 'be 3454 hj', 'vario M125'),
+('R4', 'BE 7789 MA', 'Kijangan');
 
 -- --------------------------------------------------------
 
@@ -97,8 +114,7 @@ INSERT INTO `user` (`id`, `email`, `password`, `level`) VALUES
 -- Indexes for table `tb_asset`
 --
 ALTER TABLE `tb_asset`
-  ADD PRIMARY KEY (`id_asset`),
-  ADD KEY `id_kategori` (`id_kategori`);
+  ADD PRIMARY KEY (`id_asset`);
 
 --
 -- Indexes for table `tb_kategori`
@@ -126,7 +142,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
