3540908b24b28d6e5199585364e1b87e128351bd
Masagis <31729034+Masagis@users.noreply.github.com>
2019-09-24 18:59:46 +0700

    update database

--- db_assets.sql
+++ db_assets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2019 at 07:40 PM
+-- Generation Time: Sep 24, 2019 at 12:42 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -45,8 +45,13 @@ CREATE TABLE `tb_asset` (
 --
 
 INSERT INTO `tb_asset` (`id_asset`, `nopol`, `kete_aset`, `tgl_perolehan`, `hrg_baku`, `umur_ekonomis`, `nilai_sisa`, `nilai_susut`, `kategori`) VALUES
-('R2-001', 'BE 7789 MB', 'Motor', '2019-07-09', 23000000, 15, 3000000, 0, 'R2'),
-('R2-002', 'fg 8388 kj', 'Motor', '2019-08-26', 23000000, 10, 5000000, 0, 'R2');
+('R2-001', 'BE 7789 MB', 'Yamaha Lexi-S', '2019-04-16', 1000, 10, 500, 0, 'R2'),
+('R2-002', 'fg 8388 kj', 'Yamaha NMAX 155', '2019-08-26', 23000000, 10, 5000000, 0, 'R2'),
+('R2-003', 'BE 7965 MA', 'Yamaha Aerox 155 VVA', '2019-06-04', 10000, 10, 5000, 0, 'R2'),
+('R2-004', 'BE 3333 Mc', 'Honda PCX 150', '2019-07-17', 2000, 5, 300, 0, 'R2'),
+('R2-005', 'BH 4333 MV', 'Honda Vario 150 eSP', '2019-09-10', 10000000, 10, 3000000, 0, 'R2'),
+('R4-001', 'BE 3231 MB', 'Honda Jazz MT', '2019-07-10', 120000000, 8, 4000000, 0, 'R4'),
+('R4-002', 'BE 1246 IKJ', 'Toyota Fortuner', '2019-05-08', 750000000, 7, 4500000, 0, 'R4');
 
 -- --------------------------------------------------------
 
